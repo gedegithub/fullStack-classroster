75d823f3bb9e73d816e06e56c96364975fd1f55c
Harley Cheema <harleenscheema@gmail.com>
2022-04-27 15:10:58 -0500

    new commit

--- classroster/src/main/java/com/sg/classroster/controller/StudentController.java
+++ classroster/src/main/java/com/sg/classroster/controller/StudentController.java
@@ -4,6 +4,7 @@
  */
 package com.sg.classroster.controller;
 
+import com.sg.classroster.dao.CourseDao;
 import com.sg.classroster.dao.StudentDao;
 import com.sg.classroster.dao.TeacherDao;
 import com.sg.classroster.dto.Student;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 
 /**
  *
