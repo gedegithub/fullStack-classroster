c657d3fa8a7f9dedb695fcd57bf9f4c3063de154
Harley Cheema <harleenscheema@gmail.com>
2022-04-27 14:24:09 -0500

    commit

--- classroster/src/main/java/com/sg/classroster/dto/Student.java
+++ classroster/src/main/java/com/sg/classroster/dto/Student.java
@@ -1,10 +1,71 @@
 package com.sg.classroster.dto;
 
+import java.util.Objects;
+
 /**
- * @author Ronald Gedeon; email: gedemarcel0002@hotmail.com;  
- * gitRepo: https://github.com/gedegithub/C223-JavaDev.git 
- * Design of a class ... on month day, year
+ * @author Ronald Gedeon; email: gedemarcel0002@hotmail.com; gitRepo:
+ * https://github.com/gedegithub/C223-JavaDev.git Design of a class ... on month
+ * day, year
  */
 public class Student {
 
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + this.id;
+        hash = 37 * hash + Objects.hashCode(this.firstName);
+        hash = 37 * hash + Objects.hashCode(this.lastName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        return Objects.equals(this.lastName, other.lastName);
+    }
+    
+    
 }
